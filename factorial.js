98e5ce851099e3dedec9d5f3bf217dfd7e7225f8
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-05-26 09:23:31 +0600

    update

---
+++ factorial.js
@@ -0,0 +1,12 @@
+// let result = 1;
+
+// for (let i = 1; i <= 5; i++) {
+//   result *= i;
+// }
+
+// console.log(result); // Output: 120
+let result =1;
+for(let i=1; i<=5; i++) {
+    result*=i;
+}
+document.write(result);
---
+++ for_in.js
@@ -0,0 +1,5 @@
+let data = [7,8,9,10,11]
+data.forEach(info);
+function info(p) {
+    document.write(p-2 + "<br>")
+}
