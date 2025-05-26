7883ea378fee25a554f5eb5177ece31b84622d51
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-05-26 10:44:49 +0600

    update

--- factorial.js
+++ factorial.js
@@ -5,8 +5,10 @@
 // }
 
 // console.log(result); // Output: 120
-let result =1;
-for(let i=1; i<=5; i++) {
+let result =Number(prompt("Enter your number:"));
+for(let i=1; i<=result; i++) {
     result*=i;
 }
-document.write(result);
+document.write(result)
+
+document.write("<br>")
